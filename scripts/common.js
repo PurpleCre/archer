c6cddd81d70c8034673e7a1bff4e418f7a4d29bd
PurpleCre <pgonese6@gmail.com>
2025-05-20 12:02:40 +0200

    mahwe - close

--- scripts/common.js
+++ scripts/common.js
@@ -2,6 +2,8 @@
 let lastScroll = 0;
 const header = document.getElementById("main-header");
 const logo = document.querySelector(".logo");
+const ham = document.querySelector("#menu");
+const nav = document.querySelector("nav");
 
 // header scroll listener
 window.addEventListener("scroll", () => {
@@ -24,4 +26,10 @@ window.addEventListener("scroll", () => {
   }
 
   lastScroll = currentScroll;
+});
+
+// responsive ham and nav
+ham.addEventListener("click", () => {
+  ham.classList.toggle("show");
+  nav.classList.toggle("show");
 });
