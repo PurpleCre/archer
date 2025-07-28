a46a504b44bd21c9fb8e44e43a38712b5ecf2f82
PurpleCre <pgonese6@gmail.com>
2025-07-28 17:17:11 +0200

    connected whatsapp form

---
+++ architecture/scripts/contact.js
@@ -0,0 +1,22 @@
+//  link form to whatsapp
+document
+  .getElementById("whatsapp-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const fname = document.getElementById("fname").value.trim();
+    const lname = document.getElementById("lname").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const type = document.getElementById("type").value.trim();
+    
+    const message = document.getElementById("msg").value.trim();
+    
+    const name = `${fname} ${lname}`;
+    const phoneNumber = "263776583517"; // e.g., 263771234567 (no + sign)
+
+    const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+      `Hello, my name is ${name}. I have a ${type} project, with thw following specifications. ${message}`
+    )}`;
+
+    window.open(url, "_blank");
+  });
