84c6fc1387a2d3bcebeb2fda8587bd0fb8dc47ee
PurpleCre <pgonese6@gmail.com>
2025-07-31 11:49:53 +0200

    solar mobile styles: initial

---
+++ solar/scripts/common.js
@@ -0,0 +1,36 @@
+// scroll hooks
+let lastScroll = 0;
+const header = document.getElementById  ("main-header");
+const logo = document.querySelector(".logo");
+const ham = document.querySelector("#menu");
+const nav = document.querySelector("nav");
+
+// header scroll listener
+// window.addEventListener("scroll", () => {
+//   const currentScroll = window.pageYOffset;
+  
+//   // Hide on scroll down, show on scroll up
+//   if (currentScroll > lastScroll && currentScroll > 100) {
+//     header.classList.add("hide");
+//   } else {
+//     header.classList.remove("hide");
+//   }
+
+//   lastScroll = currentScroll;
+// });
+
+// show to top button on scroll
+// window.addEventListener("scroll", () => {
+//   const toTop = document.querySelector(".top-btn");
+//   if (window.scrollY > 600) {
+//     toTop.classList.remove("hide");
+//   } else {
+//     toTop.classList.add("hide");
+//   }
+// });
+
+// responsive ham and nav
+ham.addEventListener("click", () => {
+  ham.classList.toggle("show");
+  nav.classList.toggle("show");
+});
